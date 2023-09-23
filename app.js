4caef1a2f2c34612db17b2f7fd9a27a47ec75d96
imbriankeeley <imbriankeeley@gmail.com>
2023-09-23 11:18:32 -0400

    Created set new grid button

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const grid = document.querySelector('.grid');
+const button = document.querySelector('.set-grid');
+let gridSize = 8
 
-makeGrid(8)
+makeGrid(gridSize)
 
 function makeGrid(size){
     for(let i = 0; i < size; i++){
@@ -24,3 +26,17 @@ grid.addEventListener('mouseover', function(event) {
         event.target.classList.add('active');
     }
   });
+
+button.addEventListener('click', function() {
+    gridSize = prompt('Select a grid size!\nExample: 4 for 4x4');
+    
+    reset();
+})
+
+function reset() {
+    while (grid.firstChild) {
+      grid.removeChild(grid.lastChild);
+    }
+    makeGrid(gridSize);
+  }
+  
